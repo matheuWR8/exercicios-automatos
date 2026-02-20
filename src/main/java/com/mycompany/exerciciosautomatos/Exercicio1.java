a0ae00ad4b189b66bd38c777f40a0b0b2a26f6f2
matheuwr8 <matheusswr8@gmail.com>
2026-02-19 22:30:40 -0300

    Commit inicial

---
+++ src/main/java/com/mycompany/exerciciosautomatos/Exercicio1.java
@@ -0,0 +1,78 @@
+package com.mycompany.exerciciosautomatos;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+// Implemente um AFD que aceita todas as strings binárias que terminam com 1.
+
+public class Exercicio1 {
+    
+    enum Estado {
+        Q0,
+        Q1
+    }
+ 
+    static Estado transicao(Estado estadoAtual, char simbolo) {
+
+        switch (estadoAtual) {
+
+            case Q0:
+                switch (simbolo) {
+                    case '0':
+                        return Estado.Q0;
+                    case '1':
+                        return Estado.Q1;
+                    default:
+                        throw new IllegalArgumentException("Símbolo inválido: " + simbolo);
+                }
+
+            case Q1:
+                switch (simbolo) {
+                    case '0':
+                        return Estado.Q0;
+                    case '1':
+                        return Estado.Q1;
+                    default:
+                        throw new IllegalArgumentException("Símbolo inválido: " + simbolo);
+                }
+
+            default:
+                throw new IllegalStateException("Estado inválido");
+        }
+    }
+
+    // Reconhecedor do autômato
+    static boolean reconhece(String cadeia) {
+        Estado estado = Estado.Q0; 
+
+        for (int i = 0; i < cadeia.length(); i++) {
+            estado = transicao(estado, cadeia.charAt(i));
+        }
+
+        // Estado de aceitação
+        return estado == Estado.Q1;
+    }
+    
+    public static void main(String[] args) {
+        
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma cadeia binária (0 e 1): ");
+        String cadeia = scanner.nextLine().trim();
+
+        try {
+            if (reconhece(cadeia)) {
+                System.out.println("Cadeia ACEITA");
+            } else {
+                System.out.println("Cadeia REJEITADA");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+    
+}
---
+++ src/main/java/com/mycompany/exerciciosautomatos/ExerciciosAutomatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exerciciosautomatos;
+
+/**
+ *
+ * @author 081230011
+ */
+public class ExerciciosAutomatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
